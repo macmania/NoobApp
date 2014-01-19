59c30dd80d96f4b359ef25d86fe4d2e5a12780c3
Jouella Fabe <jojofabe@vt.edu>
2014-01-19 04:01:27 -0500

    Initial Commit

---
+++ BeginnerApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeginnerApp
+//
+//  Created by Jouella Fabe on 1/19/14.
+//  Copyright (c) 2014 Jouella Fabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeginnerApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BeginnerApp
+//
+//  Created by Jouella Fabe on 1/19/14.
+//  Copyright (c) 2014 Jouella Fabe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
